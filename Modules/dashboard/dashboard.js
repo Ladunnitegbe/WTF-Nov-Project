f035610fad8ba1deb046c50037d21324685ce55d
Ladunnitegbe <93840071+Ladunnitegbe@users.noreply.github.com>
2025-12-08 01:55:00 +0100

    worked on dashboard

--- Modules/dashboard/dashboard.js
+++ Modules/dashboard/dashboard.js
@@ -1,21 +1,21 @@
-const title = document.querySelector('.dashboard-title');
+const title = document.querySelector('.dashboard_title');
 
 let gettitle = JSON.parse(localStorage.getItem('userInfo'));
 title.innerHTML = `Good evening, ${gettitle.Name}!`;
 
-const changeContent = document.querySelectorAll('.change-content');
+const changeContent = document.querySelectorAll('.change_content');
 
 const card = [
-    { title: "Total Expenses", image: "/components/card/images/dollar-icon.svg", Alt: 'dollar icon', amount: "$0.0", comment: "This period" },
-    { title: "Total Notes", image: "/components/card/images/file-icon.svg", Alt: 'file icon', amount: "0", comment: "Saved Notes" },
-    { title: "Complection Rate", image: "/components/card/images/progress-arrow.svg", Alt: 'progress arrow icon', amount: "0%", comment: "Task Complection" },
+    { title: "Total Expenses", image: "/Components/task_card/task_images/dollar sign.svg", alt: 'dollar icon', amount: "$0.0", comment: "This period" },
+    { title: "Total Notes", image: "/Components/task_card/task_images/file sign.svg", alt: 'file icon', amount: "0", comment: "Saved Notes" },
+    { title: "Complection Rate", image: "/Components/task_card/task_images/progress-arrow icon.svg", alt: 'progress arrow icon', amount: "0%", comment: "Task Complection" },
 ];
 
 changeContent.forEach((ele, i) => {
-    const cardTitle = ele.querySelector('.card-title');
-    const cardImage = ele.querySelector('.img-conatainer img'); 
-    const cardAmount = ele.querySelector('.amount-num');
-    const cardComment = ele.querySelector('.amount-comment');
+    const cardTitle = ele.querySelector('.task_card___content_title');
+    const cardImage = ele.querySelector('.task_card___img_container img'); 
+    const cardAmount = ele.querySelector('.task_card___amount_num');
+    const cardComment = ele.querySelector('.task_card___amount_comment');
 
   
     cardTitle.textContent = card[i].title;
